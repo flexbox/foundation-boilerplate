50ca9b70168ae3325df8068c495ab1bcbb53de37
David Leuliette <dleuliette@siliconsalad.com>
2015-04-13 18:44:46 +0200

    [gem] add favicon maker & refactor grid page

--- config.rb
+++ config.rb
@@ -18,6 +18,9 @@ activate :navtree do |options|
   options.ext_whitelist = [] # If you add extensions (like '.md') to this array, it builds a whitelist of filetypes for inclusion in the navtree.
 end
 
+activate :autoprefixer,
+  browsers: ['last 2 versions', 'ie 8', 'ie 9']
+
 activate :livereload
 activate :gzip
 activate :syntax
@@ -46,8 +49,32 @@ end
 
 configure :build do
 
-  activate :autoprefixer,
-    browsers: ['last 2 versions', 'ie 8', 'ie 9']
+  activate :favicon_maker do |f|
+    f.template_dir  = File.join(root, 'source')
+    f.output_dir    = File.join(root, 'build')
+    f.icons = {
+      "favicon_base.png" => [
+        { icon: "apple-touch-icon-152x152-precomposed.png" },
+        { icon: "apple-touch-icon-144x144-precomposed.png" },
+        { icon: "apple-touch-icon-120x120-precomposed.png" },
+        { icon: "apple-touch-icon-114x114-precomposed.png" },
+        { icon: "apple-touch-icon-76x76-precomposed.png" },
+        { icon: "apple-touch-icon-72x72-precomposed.png" },
+        { icon: "apple-touch-icon-60x60-precomposed.png" },
+        { icon: "apple-touch-icon-57x57-precomposed.png" },
+        { icon: "apple-touch-icon-precomposed.png", size: "57x57" },
+        { icon: "apple-touch-icon.png", size: "57x57" },
+        { icon: "favicon-196x196.png" },
+        { icon: "favicon-160x160.png" },
+        { icon: "favicon-96x96.png" },
+        { icon: "favicon-32x32.png" },
+        { icon: "favicon-16x16.png" },
+        { icon: "favicon.png", size: "16x16" },
+        { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
+        { icon: "mstile-144x144", format: "png" },
+      ]
+    }
+  end
 
   activate :minify_css
   activate :minify_javascript
