0c09ae1240a5aa4e1765d5b35d13accabe9dd2c7
David Leuliette <dleuliette@siliconsalad.com>
2015-05-11 13:59:31 +0200

    Refactor favicon images naming

--- config.rb
+++ config.rb
@@ -53,24 +53,15 @@ configure :build do
     f.output_dir    = File.join(root, 'build')
     f.icons = {
       "favicon_base.png" => [
-        { icon: "apple-touch-icon-152x152-precomposed.png" },
-        { icon: "apple-touch-icon-144x144-precomposed.png" },
-        { icon: "apple-touch-icon-120x120-precomposed.png" },
-        { icon: "apple-touch-icon-114x114-precomposed.png" },
-        { icon: "apple-touch-icon-76x76-precomposed.png" },
-        { icon: "apple-touch-icon-72x72-precomposed.png" },
-        { icon: "apple-touch-icon-60x60-precomposed.png" },
-        { icon: "apple-touch-icon-57x57-precomposed.png" },
-        { icon: "apple-touch-icon-precomposed.png", size: "57x57" },
-        { icon: "apple-touch-icon.png", size: "57x57" },
+        { icon: "chrome-touch-icon-192x192.png" },
+        { icon: "apple-touch-icon.png", size: "152x152" },
+        { icon: "ms-touch-icon-144x144-precomposed.png", size: "144x144" },
         { icon: "favicon-196x196.png" },
         { icon: "favicon-160x160.png" },
         { icon: "favicon-96x96.png" },
         { icon: "favicon-32x32.png" },
         { icon: "favicon-16x16.png" },
-        { icon: "favicon.png", size: "16x16" },
         { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
-        { icon: "mstile-144x144", format: "png" },
       ]
     }
   end
